f8edd1c2fa20ac68fa30e6f73ee23dbe318ecb3b
Icaro Conceição <perauta09@outlook.com>
2023-06-07 21:41:52 -0300

    feat: developing user services

--- src/main/java/com/example/meusgastos/domain/service/UsuarioService.java
+++ src/main/java/com/example/meusgastos/domain/service/UsuarioService.java
@@ -1,20 +1,43 @@
     package com.example.meusgastos.domain.service;
 
+    import com.example.meusgastos.domain.model.Usuario;
+    import com.example.meusgastos.domain.repository.UsuarioRepository;
     import com.example.meusgastos.dto.usuario.UsuarioRequestDto;
     import com.example.meusgastos.dto.usuario.UsuarioResponseDto;
+    import org.modelmapper.ModelMapper;
+    import org.springframework.beans.factory.annotation.Autowired;
 
+    import java.util.ArrayList;
     import java.util.List;
+    import java.util.Optional;
 
     public class UsuarioService implements ICRUDService<UsuarioRequestDto, UsuarioResponseDto> {
 
+        @Autowired
+           private UsuarioRepository usuarioRepository;
+
+        @Autowired
+        private ModelMapper mapper;
+
         @Override
         public List<UsuarioResponseDto> obterTodos() {
-            return null;
+
+            List<Usuario> usuarios = usuarioRepository.findAll();
+            List<UsuarioResponseDto> usuariosDto = new ArrayList<>();
+
+            for (Usuario usuario : usuarios) {
+                usuariosDto.add(mapper.map(usuario, UsuarioResponseDto.class));
+            }
+            return usuariosDto;
         }
 
         @Override
         public UsuarioResponseDto obterPorId(Long id) {
-            return null;
+            Optional<Usuario> optUsuario = usuarioRepository.findById(id);
+
+            if (optUsuario.isEmpty()) {
+            }
+            return mapper.map(optUsuario.get(), UsuarioResponseDto.class);
         }
 
         @Override
